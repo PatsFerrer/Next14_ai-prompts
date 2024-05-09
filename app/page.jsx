e55387f3b330b2e1cb5649c740efc83f696e3393
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-05-09 20:37:26 -0300

    iniciando page do Home

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,20 @@
+import Feed from '@components/Feed';
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Descubra e Compartilhe
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">Prompts Impulsionados por IA</span>
+      </h1>
+      <p className="desc text-center">
+        Promptsoft é uma ferramenta de sugestão de IA de código aberto para o mundo moderno descobrir, criar e compartilhar prompts criativos.
+      </p>
+
+      <Feed />
+
+    </section>
   )
 }
 
