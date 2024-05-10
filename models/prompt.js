beed6cfd8dc5cb7135fb4aa42d617aaa1e88b4a2
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-05-10 14:41:25 -0300

    prompt schema

---
+++ models/prompt.js
@@ -0,0 +1,20 @@
+import { Schema, model, models, mongo } from 'mongoose';
+
+const PromptSchema = new Schema({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  prompt: {
+    type: String,
+    required: [true, 'Prompt é obrigatório.']
+  },
+  tag: {
+    type: String,
+    required: [true, 'Tag é obrigatório.']
+  }
+});
+
+const Prompt = models.Prompt || model('Prompt', PromptSchema);
+
+export default Prompt;
